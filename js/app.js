cbf967060807cc8a4fb79e59bf0fca8bdb013191
Thomas Zegos <tomzegos97@gmail.com>
2018-04-01 14:27:44 +0300

    Added moveCounter() function
    Notice that selecting two cards means one move (countMoves += 1).

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ let cards = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt',
 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
 let open = [];
 let countMatched = 0;
+let countMoves = 0;
 
 /*
  * Display the cards on the page
@@ -42,14 +43,15 @@ function showCard(card) {
 function hideCards(card1, card2) {
     open = [];
     setTimeout(function() {
-        card1.classList.remove("open", "show");
-        card2.classList.remove("open", "show");
+        card1.classList.remove('open', 'show');
+        card2.classList.remove('open', 'show');
       }, 1000);
 }
 
 function checkCard(currentCard) {
       open.push(currentCard);
       if (open.length === 2) {
+          moveCounter();
           let recentCard = open[0];
           if (recentCard.children[0].className === currentCard.children[0].className)
               matchCards(recentCard, currentCard);
@@ -61,11 +63,24 @@ function checkCard(currentCard) {
 function matchCards(card1, card2) {
     countMatched += 1;
     open = [];
-    card1.classList.add("match");
-    card2.classList.add("match");
-    if (countMatched === 8)
-        console.log("Congrats!")
+    card1.classList.add('match');
+    card2.classList.add('match');
+    if (countMatched === cards.length/2)
+        console.log("Congrats!");
 
+}
+
+function moveCounter() {
+    countMoves += 1;
+    let movesNumber = document.querySelector(".moves");
+    let movesText = document.querySelector('.movesText');
+
+    movesNumber.textContent = countMoves;
+    if (countMoves === 1)
+        movesText.textContent = "Move";
+    else {
+      movesText.textContent = "Moves";
+    }
 }
 
 /*
